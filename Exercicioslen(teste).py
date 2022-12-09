90bed5973987894a343b42d0b7364b9079eb9cc3
GILBERTO <106849333+Giliarde0@users.noreply.github.com>
2022-12-09 20:10:50 -0300

    Delete Exercicioslen(teste).py

--- Exercicioslen(teste).py
+++
@@ -1,6 +0,0 @@
-cor = ["azul ","vermelho", "laranja", "amarelo", "azul"]
-print(len(cor))
-frutas = ["laranja", "abacaxi","melancia", "uva"]
-print(len(frutas))
-print()
-print("A função LEN ela serve para contar e demonstrar a quantidade de elementos dentro da propria lista")
